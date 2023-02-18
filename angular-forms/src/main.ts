1a45609ca904c699195be187070356eac61369a2
wilsonnovaes0000 <69542312+wilsonnovaes0000@users.noreply.github.com>
2023-02-18 09:19:38 -0300

    Alias para pasta store

--- angular-forms/src/main.ts
+++ angular-forms/src/main.ts
@@ -4,7 +4,6 @@ import { provideRouter } from '@angular/router';
 import { APP_ROUTES } from './app/app-routing.module';
 import { AppComponent } from './app/app.component';
 
-
 bootstrapApplication(AppComponent, {
   providers: [provideRouter(APP_ROUTES)]
 })
